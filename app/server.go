e84a95a45d1cd184e756a773b01947b5c45dce4a
sogafish <kent.soga@gmail.com>
2024-03-23 10:36:50 +0900

    :sparkles: Load Env

--- app/server.go
+++ app/server.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/joho/godotenv"
+)
+
 func main() {
+	loadEnv()
+	println("Server.go")
+}
+
+func loadEnv() {
+	err := godotenv.Load("../.env")
+
+	if err != nil {
+		fmt.Printf("Load Failed ENV file %v", err)
+	}
 }
